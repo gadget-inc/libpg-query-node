6715fc4ca3c6f8b2ef8ce4a28e0c1858795890af
Harry Brundage <harry.brundage@gmail.com>
2023-12-13 17:51:35 -0500

    gitpkg

---
+++ osx-build/Release/.deps/libpg_query/include/pg_query.h.d
@@ -0,0 +1,1 @@
+cmd_libpg_query/include/pg_query.h := LD_LIBRARY_PATH=/Users/airhorns/Code/libpg-query-node/build/Release/lib.host:/Users/airhorns/Code/libpg-query-node/build/Release/lib.target:$$LD_LIBRARY_PATH; export LD_LIBRARY_PATH; cd ../.; mkdir -p libpg_query/include; script/buildAddon.sh
